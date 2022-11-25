62bb2fee244385083dfafca6ea92ab37b0ce7fd4
[rrrchen] <[chengehu980726@gmail.com]>
2022-11-25 12:42:53 +0000

    test for view of barchart

--- app/tests/test_case/test_views.py
+++ app/tests/test_case/test_views.py
@@ -3,7 +3,7 @@ sys.path.append('../../..')
 
 import unittest
 
-from app.views.controllers import generate_data_for_tiles, generate_description_for_top_item,generate_data_for_unique_items,generate_percentageof_all_infection_treatments
+from app.views.controllers import generate_data_for_tiles, generate_description_for_top_item,generate_data_for_unique_items,generate_percentageof_all_infection_treatments,generate_antibiotics_barchart_data
 
 
 class DatabaseTests(unittest.TestCase):
@@ -19,6 +19,9 @@ class DatabaseTests(unittest.TestCase):
 
     # def test_generate_percentageof_all_infection_treatments(self):
     #     self.assertEquals(generate_percentageof_all_infection_treatments(), [82.25, 5.22, 2.68, 9.62, 0.23])
+
+    def test_generate_antibiotics_barchart_data(self):
+        self.assertListEqual(generate_antibiotics_barchart_data("RXA"),[[408, 30], ['Y00327', 'Y04109']])
 
 
 if __name__ == "__main__":
